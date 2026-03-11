b363d2580cb660a0a7dc686ee10e5429b3f61e06
arpitvirk07 <arpitvirk07@gmail.com>
2026-03-11 19:15:13 +0530

    Add UI endpoint to serve index.html
    Added UI endpoint to serve index.html file.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from agent import run_agent
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"message": "FireReach Agent Running"}
 
 
+# UI PAGE
+@app.get("/ui")
+def ui():
+    return FileResponse("index.html")
+
+
+# AGENT API
 @app.get("/run-agent")
 def execute(icp: str, company: str, email: str):
 
