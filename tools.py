0d151e969d233c8f0c7c677261f837f6364e7ec4
arpitvirk07 <arpitvirk07@gmail.com>
2026-03-11 18:38:40 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -3,11 +3,11 @@ from groq import Groq
 import smtplib
 from email.mime.text import MIMEText
 
-# Initialize Groq client using environment variable
+# Initialize Groq client
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 
-# TOOL 1: Signal Harvester
+# Tool 1: Signal Harvester
 def tool_signal_harvester(company):
 
     signals = [
@@ -19,7 +19,7 @@ def tool_signal_harvester(company):
     return signals
 
 
-# TOOL 2: Research Analyst
+# Tool 2: Research Analyst
 def tool_research_analyst(icp, company, signals):
 
     prompt = f"""
@@ -32,8 +32,8 @@ Signals:
 {signals}
 
 Write a short account brief explaining:
-1. company growth
-2. why cybersecurity training helps them
+1) company growth
+2) why cybersecurity training helps them
 """
 
     response = client.chat.completions.create(
@@ -44,7 +44,7 @@ Write a short account brief explaining:
     return response.choices[0].message.content
 
 
-# TOOL 3: Automated Email Sender
+# Tool 3: Automated Email Sender
 def tool_outreach_automated_sender(receiver_email, message):
 
     sender_email = os.getenv("SENDER_EMAIL")
